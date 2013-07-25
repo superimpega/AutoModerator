eb547ab871f6b8f4341b8567e5ea30e9495a6bf6
superimpega <awesomealexmeh1@hotmail.co.uk>
2013-07-25 11:46:08 +0100

    Update AM.js

--- AM.js
+++ AM.js
@@ -3,7 +3,7 @@ if (autoModerator !== undefined)
 
 String.prototype.equalsIgnoreCase     = function(other)    { return typeof other !== 'string' ? false : this.toLowerCase() === other.toLowerCase(); };
 var autoModeratorModel = require('app/base/Class').extend({
-	version: "1.0.3",
+	version: "1.0.5",
 	bannedWords: [
 		'huehue',
 		'hu3hu3',
